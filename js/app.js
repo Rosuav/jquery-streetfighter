de702702d3537ba46b35f849ee85e00d45a172e2
Chris Angelico <rosuav@gmail.com>
2016-06-13 21:33:52 +1000

    (Oops, double) Add hadouken sound and cool mode

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     $('.ryu-still').show();
   })
   .mousedown(function() {
-    // play hadouken sound
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show().animate(
@@ -24,4 +24,24 @@ $(document).ready(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
+
+  $('body').keydown(function() {
+    if (event.which == 88) {
+      $('.ryu-still, .ryu-ready').hide();
+      $('.ryu-cool').show();
+    }
+  })
+  .keyup(function() {
+    if (event.which == 88) {
+      $('.ryu-still').show();
+      $('.ryu-cool').hide();
+    }
+  });
 });
+
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
